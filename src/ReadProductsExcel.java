fc4457c431ed739277fa7bd57f246dd9f103a172
“e <1322291822@qq.com>
2020-12-30 17:19:59 +0800

    1、在Readproduct类中添加一个通过ID查找商品的方法即getProductByID 2、增加了通过商品ID查找商品的功能。 3、新增了购物车功能，通过输入商品ID加入到购物车

--- src/ReadProductsExcel.java
+++ src/ReadProductsExcel.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
 
-public class ReadProductExcel {
+public class ReadProductsExcel {
     public Product[] readExcel(InputStream Inproduct) {
         Product products[] = null;
 
@@ -43,6 +43,41 @@ public class ReadProductExcel {
         }
         return products;
     }
+    public Product getProductByID(int ID, InputStream Inproduct) {
+            Product products[] = null;
+
+
+            try {
+
+                XSSFWorkbook xw = new XSSFWorkbook(Inproduct);
+                XSSFSheet xs = xw.getSheetAt(0);
+                products = new Product[xs.getLastRowNum()];
+                for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                    XSSFRow row = xs.getRow(j);
+                    Product product = new Product();//每循环一次就把电子表格的一行的数据给对象赋值
+                    for (int k = 0; k <= row.getLastCellNum(); k++) {
+                        XSSFCell cell = row.getCell(k);
+                        if (cell == null)
+                            continue;
+                        if (k == 0) {
+                            product.setPID(Integer.parseInt(this.getValue(cell)));//给username属性赋值
+                        } else if (k == 1) {
+                            product.setPname(this.getValue(cell));//给password属性赋值
+                        } else if (k == 2) {
+                            product.setPcost(Float.parseFloat(this.getValue(cell)));//给address属性赋值
+                        } else if (k == 3) {
+                            product.setPdescri(this.getValue(cell));//给phone属性赋值
+                        }
+                    }
+                    if (ID == product.getPID())
+                        return product;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
 
     private String getValue(XSSFCell cell) {
         String value;
--- src/Test.java
+++ src/Test.java
@@ -20,13 +20,13 @@ public class Test {
             InputStream Inuser = Class.forName("Test").getResourceAsStream("shuju.xlsx");
             InputStream Inproduct=Class.forName("Test").getResourceAsStream("/Products.xlsx");
             ReadUserExcel readExcel = new ReadUserExcel();
-            ReadProductExcel readProductExcel=new ReadProductExcel();
+            ReadProductsExcel readProductExcel=new ReadProductsExcel();
             User users[] = readExcel.readExcel(Inuser);
 
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println(("登录成功"));
-                    bo = false;
+
                     /*
                     显示商品
                      */
@@ -37,7 +37,55 @@ public class Test {
                         System.out.print("\t"+p.getPcost());
                         System.out.println("\t"+p.getPdescri());
                     }
+                    System.out.println("请输入商品的ID加入购物车");
+                    int pid=sc.nextInt();
+                    int count=0;
+                    Product carts[]=new Product[3];
+                    Inproduct=null;
+                    Inproduct=Class.forName("Test").getResourceAsStream("/Products.xlsx");
+                    Product product=readProductExcel.getProductByID(pid,Inproduct);
+                    System.out.println(("要购买的商品的价格为：" + product.getPcost()));
+
+                    if(product!=null)
+                        carts[count++]=product;
+                    System.out.println(("继续购物请按1"));
+                    System.out.println(("查看购物车请按2"));
+                    int choose=sc.nextInt();
+                    /*
+                    继续购物
+                     */
+
+                    if(choose==1){
+                        Inproduct=null;
+                        Inproduct=Class.forName("Test").getResourceAsStream("/Products.xlsx");
+                        readProductExcel=new ReadProductsExcel();
+                        products=readProductExcel.readExcel(Inproduct);
+                        for(Product p:products){
+                            System.out.print("\t"+p.getPID());
+                            System.out.print("\t"+p.getPname());
+                            System.out.print("\t"+p.getPcost());
+                            System.out.println("\t"+p.getPdescri());
+                        }
+
+                        System.out.println(("输入商品ID加入购物车："));
+                        pid=sc.nextInt();
+                        Inproduct=null;
+                        Inproduct=Class.forName("Test").getResourceAsStream("/Products.xlsx");
+                        product=readProductExcel.getProductByID(pid,Inproduct);
+                        System.out.println("要购买的商品的价格为：" + product.getPcost());
+
+                        if(product!=null){
+
+                            carts[count++]=product;
+                        }
+
+
+                    }
+                    bo = false;
                     break;
+
+
+
                 }else {
                     System.out.println("登录失败");
 
